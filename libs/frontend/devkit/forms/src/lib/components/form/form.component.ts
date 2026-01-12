2668328d2add04c249c5aa7d4c3894e6b6566180
Carlos Garcia <carlos.garcia@soprasteria.com>
2026-01-12 16:31:03 +0100

    fix import

--- libs/frontend/devkit/forms/src/lib/components/form/form.component.ts
+++ libs/frontend/devkit/forms/src/lib/components/form/form.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormController } from '../../types/form-controller';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'lib-form',
-  imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule ],
+  imports: [ReactiveFormsModule, FormsModule,  MatInput, MatFormFieldModule ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
