c31c178fece412ca4cff007d28dfd9c429b3e2fa
cgarcia0910 <cgarcia0910@gmail.com>
2026-02-13 10:57:29 +0100

    Refactor module federation config for shell app
    Uncomment and update module federation configuration with remotes and shared settings.

--- apps/frontend/shell/shell/module-federation.config.ts
+++ apps/frontend/shell/shell/module-federation.config.ts
@@ -1,4 +1,4 @@
-const isProd = process.env['NODE_ENV'] === 'production';
+/*const isProd = process.env['NODE_ENV'] === 'production';
 
 const config: ModuleFederationConfig = {
   name: 'shell',
@@ -22,4 +22,39 @@ const config: ModuleFederationConfig = {
   }
 };
 
+export default config;
+*/
+
+import { ModuleFederationConfig } from '@nx/module-federation';
+
+const config: ModuleFederationConfig = {
+  name: 'shell',
+  /**
+   * To use a remote that does not exist in your current Nx Workspace
+   * You can use the tuple-syntax to define your remote
+   *
+   * remotes: [['my-external-remote', 'https://nx-angular-remote.netlify.app']]
+   *
+   * You _may_ need to add a `remotes.d.ts` file to your `src/` folder declaring the external remote for tsc, with the
+   * following content:
+   *
+   * declare module 'my-external-remote';
+   *
+   */
+  remotes: ['config', 'planning'],
+  shared: (libraryName, defaultConfig) => {
+    if (libraryName === '@ngneat/transloco') {
+      return {
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto'
+      };
+    }
+    return defaultConfig;
+  }
+};
+
+/**
+ * Nx requires a default export of the config to allow correct resolution of the module federation graph.
+ **/
 export default config;
