c178cc2a80d284cfe0a31b615b3da94692b3ad02
cgarcia0910 <cgarcia0910@gmail.com>
2026-02-13 09:49:54 +0100

    Update main.ts

--- apps/frontend/shell/shell/src/main.ts
+++ apps/frontend/shell/shell/src/main.ts
@@ -1,7 +1,12 @@
 import { setRemoteDefinitions } from '@nx/angular/mf';
+import { loadRemoteDefinitions } from '@nx/angular/mf';
 
-fetch('/assets/module-federation.manifest.json')
+loadRemoteDefinitions('/assets/module-federation.manifest.json')
+  .then(() => import('./bootstrap'))
+  .catch((err) => console.error(err));
+
+/*fetch('/assets/module-federation.manifest.json')
   .then(res => res.json())
   .then(definitions => setRemoteDefinitions(definitions))
   .then(() => import('./bootstrap'))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err));*/
