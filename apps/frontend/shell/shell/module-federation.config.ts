1dd60fd5646df36710665ac8b6bc5f6626706793
cgarcia0910 <cgarcia0910@gmail.com>
2026-02-13 09:56:15 +0100

    Update module federation config for production remotes
    Refactor module federation configuration to conditionally set remotes based on environment.

--- apps/frontend/shell/shell/module-federation.config.ts
+++ apps/frontend/shell/shell/module-federation.config.ts
@@ -1,20 +1,15 @@
-import { ModuleFederationConfig } from '@nx/module-federation';
+const isProd = process.env['NODE_ENV'] === 'production';
 
 const config: ModuleFederationConfig = {
   name: 'shell',
-  /**
-   * To use a remote that does not exist in your current Nx Workspace
-   * You can use the tuple-syntax to define your remote
-   *
-   * remotes: [['my-external-remote', 'https://nx-angular-remote.netlify.app']]
-   *
-   * You _may_ need to add a `remotes.d.ts` file to your `src/` folder declaring the external remote for tsc, with the
-   * following content:
-   *
-   * declare module 'my-external-remote';
-   *
-   */
-  remotes: ['config', 'planning'],
+
+  remotes: isProd
+    ? [
+        ['config', 'https://config.onrender.com'],
+        ['planning', 'https://planning.onrender.com'],
+      ]
+    : ['config', 'planning'],
+
   shared: (libraryName, defaultConfig) => {
     if (libraryName === '@ngneat/transloco') {
       return {
@@ -27,7 +22,4 @@ const config: ModuleFederationConfig = {
   }
 };
 
-/**
- * Nx requires a default export of the config to allow correct resolution of the module federation graph.
- **/
 export default config;
