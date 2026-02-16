e5c2a40e6120301bca81c9873b8c25dbf7da566f
Carlos Garcia <carlos.garcia@soprasteria.com>
2026-02-16 09:24:55 +0100

    fix error

--- apps/frontend/shell/shell/webpack.prod.config.ts
+++ apps/frontend/shell/shell/webpack.prod.config.ts
@@ -17,8 +17,8 @@ export default async function (webpackConfig: Configuration, options: any) {
        * Each entry is a pair of a unique name and the URL where it is deployed.
        */
       remotes: [
-        ['config', 'process.env.MF_CONFIG_URL'],
-        ['planning', 'process.env.MF_PLANNING_URL'],
+        ['config', process.env.MF_CONFIG_URL || ''],
+        ['planning', process.env.MF_PLANNING_URL || ''],
       ],
     },
     { dts: false }
